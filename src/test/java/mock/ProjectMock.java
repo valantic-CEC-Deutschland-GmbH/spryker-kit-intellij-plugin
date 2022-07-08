19a2ec8ff663d3447a207a41d6df47b795f7bfcd
Jonas Hoppe <pwnyprod@gmail.com>
2022-07-08 16:57:32 +0200

    #1 adding unittest step and fixup ProjectMock

--- src/test/java/mock/ProjectMock.java
+++ src/test/java/mock/ProjectMock.java
@@ -1,12 +1,20 @@
 package mock;
 
+import com.intellij.diagnostic.ActivityCategory;
+import com.intellij.openapi.extensions.PluginDescriptor;
+import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.messages.MessageBus;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.SystemIndependent;
 import org.picocontainer.PicoContainer;
+
+import java.util.Map;
 
 public class ProjectMock<T> implements Project {
 
@@ -16,7 +24,7 @@ public class ProjectMock<T> implements Project {
         this.serviceToReturn = serviceToReturn;
     }
 
-    public T getService(Class theClass) {
+    public T getService(@NotNull Class theClass) {
         return this.serviceToReturn;
     }
 
@@ -83,8 +91,18 @@ public class ProjectMock<T> implements Project {
     }
 
     @Override
+    public <T> T @NotNull [] getComponents(@NotNull Class<T> baseClass) {
+        return null;
+    }
+
+    @Override
     public @org.jetbrains.annotations.NotNull PicoContainer getPicoContainer() {
         return null;
+    }
+
+    @Override
+    public boolean isInjectionForExtensionSupported() {
+        return false;
     }
 
     @Override
@@ -99,6 +117,36 @@ public class ProjectMock<T> implements Project {
 
     @Override
     public @org.jetbrains.annotations.NotNull Condition<?> getDisposed() {
+        return null;
+    }
+
+    @Override
+    public <T> T instantiateClassWithConstructorInjection(@NotNull Class<T> aClass, @NotNull Object key, @NotNull PluginId pluginId) {
+        return null;
+    }
+
+    @Override
+    public @NotNull RuntimeException createError(@NotNull Throwable error, @NotNull PluginId pluginId) {
+        return null;
+    }
+
+    @Override
+    public @NotNull RuntimeException createError(@NotNull @NonNls String message, @NotNull PluginId pluginId) {
+        return null;
+    }
+
+    @Override
+    public @NotNull RuntimeException createError(@NotNull @NonNls String message, @Nullable Throwable error, @NotNull PluginId pluginId, @Nullable Map<String, String> attachments) {
+        return null;
+    }
+
+    @Override
+    public @NotNull <T> Class<T> loadClass(@NotNull String className, @NotNull PluginDescriptor pluginDescriptor) throws ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public @NotNull ActivityCategory getActivityCategory(boolean isExtension) {
         return null;
     }
 
